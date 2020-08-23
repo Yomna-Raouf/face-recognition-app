9a758382930cbe00b8e8e812db6f5d085c700610
yomna raouf <yomnaraouf99@gmail.com>
2020-08-23 11:50:28 -0700

    add:Navbar skeleton

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+           <button className="Navbar__signout">Sign out</button>
+        </nav>
+    )
+}
+
+export default Navbar
