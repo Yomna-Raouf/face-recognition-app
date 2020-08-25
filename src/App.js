a75127e60176e04d4e005b26e3615f8ad1aa958f
yomna raouf <yomnaraouf99@gmail.com>
2020-08-25 12:03:19 -0700

    main page Done & made responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,80 @@
 import React from 'react';
 import './App.css';
+import Particles from "react-tsparticles";
 import Navbar from './Components/Navbar/Navbar';
 import Logo from './Components/Logo/Logo';
 import LinkForm from './Components/LinkForm/LinkForm';
+import Rank from './Components/Rank/Rank';
 
 const App = () => {
+
+  const ParticlesOptions = {
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+    },
+    detectRetina: true,
+  }
+
   return (
     <div className="App">
+     <Particles className="particles" params={ ParticlesOptions } />
      <Navbar/>
      <Logo />
+     <Rank />
      <LinkForm />
     </div>
   );
---
+++ src/Components/Rank/Rank.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Rank.css';
+
+function Rank() {
+    return (
+        <div className="Rank">
+            <div className="Rank__user">
+                <p>Yomna, your Rank is </p>
+            </div>
+            <div className="Rank__score">
+                <p>5</p>
+            </div>
+        </div>
+    )
+}
+
+export default Rank
