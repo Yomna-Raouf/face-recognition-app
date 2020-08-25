7a2522e48cbb8a27fdc0a81ef8acdcf7ac93b8f2
yomna raouf <yomnaraouf99@gmail.com>
2020-08-24 22:13:03 -0700

    add: LinkForm

--- src/Components/LinkForm/LinkForm.jsx
+++ src/Components/LinkForm/LinkForm.jsx
@@ -5,8 +5,14 @@ function LinkForm() {
     return (
         <div className="LinkForm">
             <p className="LinkForm__description">
-
+                { 'This App will detect faces in your pictures. Give it a try' }
             </p>
+            <div className="LinkForm__formContainer">
+                <form className="LinkForm__form">
+                    <input placeholder="Your Image Link" className="LinkForm__formInput" type="text" />
+                    <button className="LinkForm__formButton">Detect</button>
+                </form>
+            </div>
         </div>
     )
 }
