03cba459cc38ff8047831ec5144f8d85c368336e
yomna raouf <yomnaraouf99@gmail.com>
2020-10-09 05:42:35 -0700

    Updates

---
+++ precache-manifest.3fcbc36b75ee52b9c3b665904f0bf92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb0190d73cb5c5bcdeffce8d97fd46",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "d5d3f2e11f7d04eb7dc1",
+    "url": "/face-recognition-app/static/css/main.2c21d4e2.chunk.css"
+  },
+  {
+    "revision": "161f2f345893366ea3ea",
+    "url": "/face-recognition-app/static/js/2.b855a8a3.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/face-recognition-app/static/js/2.b855a8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d3f2e11f7d04eb7dc1",
+    "url": "/face-recognition-app/static/js/main.fa555fc1.chunk.js"
+  },
+  {
+    "revision": "dd7f0eb76da148b88c1e",
+    "url": "/face-recognition-app/static/js/runtime-main.b2d6d78d.js"
+  },
+  {
+    "revision": "6d0e212abed92c965036e2b2070aea45",
+    "url": "/face-recognition-app/static/media/identity.6d0e212a.svg"
+  }
+]);
